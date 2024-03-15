d744e8073e603f1a958b14ef34287f5f38bf21b6
Belle2771 <163418288+Belle2771@users.noreply.github.com>
2024-03-15 10:06:48 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src="https://veja.abril.com.br/wp-content/uploads/2016/06/alx_entretenimento-filmes-mais-caros-enrolados-20131210-001_original.jpeg?quality=90&strip=info&w=1024&resize=1200,800"
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://miro.medium.com/v2/resize:fit:2800/0*GofZlIlAA-UDyUJO.jpg";
+}
